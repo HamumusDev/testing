2bb4e9fb0c89854765348833683a3eb99a747205
HamumusDev <csaki.boldizsar.developer@gmail.com>
2026-06-30 23:04:15 +0200

    Move Jest config to jest.config.ts and jest-e2e.config.ts

---
+++ jest-e2e.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+  },
+  testEnvironment: 'node',
+};
+
+export default config;
